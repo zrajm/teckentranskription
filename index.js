e9a0e0aa552ab9faae5b88ce85bf452ca073adaa
zrajm <zrajm@klingonska.org>
2017-09-17 13:28:36 +0200

    Textarea now keeps focus when pressin buttons.

--- index.js
+++ index.js
@@ -25,10 +25,32 @@ function insertAtCursor(domElement, str) {
 }
 
 var textareaElem = $('textarea').get(0);
-$('button').click(function () {
-    // string inside button (stripping off any &nbsp; + ◌).
-    var str = $(this).html().replace(/(&nbsp;|◌)/g, '');
-    insertAtCursor(textareaElem, str);
-});
+$('button').mousedown(button_clicked).keydown(button_clicked);
+
+// Invoke this on .mousedown() and/or .keydown() (to catch mouse clicks and
+// keyboard input respectively). This will prevent default function of an
+// element it is attached to (specifically, if attached to .mousedown(), this
+// will prevent the button from being focused when clicked -- so that the
+// previously focused element keeps its focus; this will not worked for
+// .click() as the element is already focused when that event is triggered).
+function button_clicked(event) {
+    var key;
+
+    // Abort if qualifier key was pressed.
+    if (event.shiftKey || event.ctrlKey || event.altKey || event.metaKey) {
+        return true;
+    }
+
+    // Pressed: Left mouse button, enter or space.
+    key = event.which;
+    if (key === 1 || key === 13 || key === 32) {
+        event.preventDefault();
+
+        // string inside button (stripping off any &nbsp; + ◌).
+        var str = $(this).html().replace(/(&nbsp;|◌)/g, '');
+        insertAtCursor(textareaElem, str);
+    }
+    return true;
+}
 
 /*[eof]*/
