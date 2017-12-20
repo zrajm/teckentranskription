db6fed0d84ba6d4c96720724ba347b9c0c3960b3
zrajm <zrajm@klingonska.org>
2017-12-20 18:49:06 +0100

    Bugfix for onscreen keyboard.

--- keyboard.js
+++ keyboard.js
@@ -1,32 +1,41 @@
 // jqText is a jQuery DOM element in which to insert text (e.g. <textarea> or
 // <input> element).
 function transcriptKeyboard(jqText) {
-    var domText = jqText.get(0), jqKeyboard = $('#keyboard')
+    var domText = jqText.get(0), jqKeyboard = $('#keyboard'), jqInput = $('#input')
 
     // For inserting text in a textarea.
     function insertAtCursor(str) {
-        var sel, begPos, endPos, value
+        var sel, begPos, endPos, newPos, value,
+            domFocused = document.activeElement, refocused = false
+
+        // Focus text input element (if not already focused).
+        if (domFocused !== domText) {
+            domText.focus()
+            refocused = true
+        }
+
+        // Edit input field.
         if (document.selection) {
             // IE support
-            domText.focus()
             sel = document.selection.createRange()
             sel.text = str
         } else if (domText.selectionStart || domText.selectionStart == '0') {
             // MOZILLA and others
             begPos = domText.selectionStart
             endPos = domText.selectionEnd
+            newPos = begPos + str.length
             value  = domText.value
-            domText.value = value.substring(0, begPos)
-                + str
-                + value.substring(endPos, value.length)
-            setTimeout(function () {
-                domText.selectionStart = begPos + str.length
-                domText.selectionEnd   = begPos + str.length
-            }, 0)
+
+            // Replace string & move cursor.
+            domText.value = value.slice(0, begPos) + str + value.slice(endPos)
+            domText.setSelectionRange(newPos, newPos)
         } else {
             // other
             domText.value += str
         }
+
+        // Refocus original element.
+        if (refocused) { domFocused.focus() }
     }
 
     // Invoke this on .mousedown() and/or .keydown() (to catch mouse clicks and
@@ -35,30 +44,32 @@ function transcriptKeyboard(jqText) {
     // will prevent the button from being focused when clicked -- so that the
     // previously focused element keeps its focus; this will not worked for
     // .click() as the element is already focused when that event is triggered).
-    function button_clicked(event) {
-        var key
-        if (event.shiftKey || event.ctrlKey || event.altKey || event.metaKey) {
-            return true
-        }
+    function button_clicked(e) {
+        if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) { return true }
 
-        // Pressed: Left mouse button, enter or space.
-        key = event.which
-        if (key === 1 || key === 13 || key === 32) {
-            event.preventDefault()
-
-            // string inside button (stripping off any &nbsp; + ◌).
-            var str = $(this).html().replace(/(&nbsp;|◌)/g, '')
-            insertAtCursor(str)
+        switch (e.which) {
+        case 27:                     // Escape
+            e.preventDefault()
+            jqKeyboard.hide()
+            jqText.focus()
+            break
+        case 1:                      // Left mouse button
+        case 13:                     // Enter
+        case 32:                     // Space
+            e.preventDefault()
+            if ($(e.target).is('button')) {
+                // String inside button (stripping off any &nbsp; + ◌).
+                var str = $(e.target).html().replace(/(&nbsp;|◌)/g, '')
+                insertAtCursor(str)
+            }
         }
         return true
     }
 
     $(function () {
         jqKeyboard.
-            width(jqText.width()).
-            find('button').
-                mousedown(button_clicked).
-                keydown(button_clicked)
+            mousedown(button_clicked).
+            keydown(button_clicked)
         jqText.
             keydown(function (e) {
                 switch (e.key) {
@@ -71,13 +82,13 @@ function transcriptKeyboard(jqText) {
                     jqText.change()
                 }
             }).
-            change(function() { jqKeyboard.hide() })
-        $(document).click(function (e) {
-            if (e.target === domText) {
-                // click in text input area
-                jqKeyboard.show()
-            } else if ($(e.target).closest(jqKeyboard).length === 0) {
-                // click outside virtual keyboard
+            change(function() { jqKeyboard.hide() }).
+            click(function()  { jqKeyboard.show() })
+
+        // Hide screen keyboard if focus goes outside it or text field.
+        jqInput.focusout(function (e) {
+            var domFocused = $(e.relatedTarget)
+            if (domFocused.closest(jqInput, jqInput).length === 0) {
                 jqKeyboard.hide()
             }
         })
