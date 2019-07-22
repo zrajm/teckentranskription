01605f70fb69194994efa389605555f79bb99733
zrajm <zrajm@klingonska.org>
2019-07-22 09:00:37 +0200

    Imported data from official lexicon 2019-07-22

--- lexicon-data.js
+++ lexicon-data.js
@@ -1,4 +1,4 @@
-var lexiconDate = new Date("2019-07-15T06:00:27Z");
+var lexiconDate = new Date("2019-07-22T06:00:32Z");
 var lexicon = [
 ["00002","􌤀􌤢􌥓􌥘􌥲􌦀􌥼􌤟","dop","döpa"],
 ["00003","􌤀􌤢􌥓􌥙􌤟􌥼􌥻","mössa"],
