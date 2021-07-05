c397b32885fd1bc493b2610c273b07be5883406f
zrajm <zrajm@klingonska.org>
2021-07-05 09:00:29 +0200

    Imported data from official lexicon 2021-07-05

--- lexicon-data.js
+++ lexicon-data.js
@@ -1,4 +1,4 @@
-var lexiconDate = new Date("2021-06-28T06:00:34Z");
+var lexiconDate = new Date("2021-07-05T06:00:33Z");
 var lexicon = [
 ["00001","􌤀􌦫􌥓􌥘􌤟􌥼􌥻","taxi"],
 ["00002","􌤀􌤢􌥓􌥘􌥲􌦀􌥼􌤟","dop","döpa"],
